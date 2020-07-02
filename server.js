e53e4833d0bce7e776ed8324a7d50caa567d3bc3
Alexis Mollenkopf <alexismollenkopf@gmail.com>
2020-07-02 13:30:21 -0400

    basic server is running and displaying what port its on

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
