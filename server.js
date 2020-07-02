8e9ebacebbddbe0d5799737e17c2932434a1b99e
Alexis Mollenkopf <alexismollenkopf@gmail.com>
2020-07-02 14:04:22 -0400

    added a watch script tp package.json

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const path = require("path");
+const { table } = require("console");
 
-var app = express();
-var PORT = 3000;
+const app = express();
+const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-  
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
