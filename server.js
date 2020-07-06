a41c3221836bd82d8b4302063f4c0d6db46077ac
Alexis Mollenkopf <alexismollenkopf@gmail.com>
2020-07-06 10:44:29 -0400

    i havent changed naything but i have to cmmit to pull

--- server.js
+++ server.js
@@ -7,45 +7,44 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-  const table = [
-    {
-      name: "bob",
-      phone: 23141234,
-      email: "bob@gmail.com",
-      id: 12,
-    },
-  ];
-  const wait = [
-    {
-      name: "bob324",
-      phone: 23141234,
-      email: "bob@gmail.com",
-      id: 12,
-    },
-  ];
+const table = [
+  {
+    name: "bob",
+    phone: 23141234,
+    email: "bob@gmail.com",
+    id: 12,
+  },
+];
+const wait = [
+  {
+    name: "bob324",
+    phone: 23141234,
+    email: "bob@gmail.com",
+    id: 12,
+  },
+];
 
-  // Routes
-  // =================================================
+// Routes
+// =================================================
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-  app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+app.get("/table", function (req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
 
-  app.get("/table", function (req, res) {
-    res.sendFile(path.join(__dirname, "tables.html"));
-  });
+app.get("/reserve", function (req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
 
-  app.get("/reserve", function (req, res) {
-    res.sendFile(path.join(__dirname, "reserve.html"));
-  });
+app.get("/api/table", (req, res) => {
+  return res.json(table);
+});
+app.get("/api/wait", (req, res) => {
+  return res.json(wait);
+});
 
-  app.get("/api/table", (req, res) => {
-    return res.json(table);
-  });
-  app.get("/api/wait", (req, res) => {
-    return res.json(wait);
-  });
-
-  app.listen(PORT, () => {
-    console.log("App listening on PORT " + PORT);
-  });
+app.listen(PORT, () => {
+  console.log("App listening on PORT " + PORT);
+});
